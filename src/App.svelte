f2d3bef5cb7e934a7f0f43cb2d45d29df359ef6a
robwatsongtr <rwatso@gmail.com>
2021-11-18 23:59:17 -0800

    svelte project

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
-	export let name;
+	let name = 'John Doe'
+	let points = 100;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{name}</h1>
+	<h3>{points}</h3>
 </main>
 
 <style>
