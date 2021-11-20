f57ad48f0bdd5fcc6493f256c6828644966880b7
robwatsongtr <rwatso@gmail.com>
2021-11-19 22:12:23 -0800

    did some buttons and state and shit

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
 	let name = 'John Doe'
 	let points = 100;
+
+	const addPoint = () => { points += 1 }; 
+	const removePoint = () => { points -= 1 }; 
+
 </script>
 
 <style>
@@ -13,8 +17,11 @@
 
 <div class="container">
 	<div class="card">
-			<h1>{name}</h1>
-			<h3>{points}</h3>
+		<h1>{name}</h1>
+		<h3>{points}</h3>
+		<button class="btn" on:click={addPoint}>+1</button>
+		<button class="btn btn-dark" on:click={removePoint}>-1</button>
+		<input type="number" bind:value={points}>
 	</div>
 </div>
 
