3be71ee2cd37c96ac777c2c5e929bcfd5a1b73a5
robwatsongtr <rwatso@gmail.com>
2021-11-19 16:40:07 -0800

    basic styling

--- src/App.svelte
+++ src/App.svelte
@@ -3,29 +3,18 @@
 	let points = 100;
 </script>
 
-<main>
-	<h1>{name}</h1>
-	<h3>{points}</h3>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #204f6e;
 		font-size: 4em;
 		font-weight: 100;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+<div class="container">
+	<div class="card">
+			<h1>{name}</h1>
+			<h3>{points}</h3>
+	</div>
+</div>
+
