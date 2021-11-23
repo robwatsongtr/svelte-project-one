d6dfa7aba14b9233bd04040dd8ec070ca1568cd6
robwatsongtr <rwatso@gmail.com>
2021-11-22 20:15:15 -0800

    added conditional and toggling

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
 	let name = 'John Doe'
 	let points = 100;
+	let showControls = false; 
 
 	const addPoint = () => { points += 1 }; 
 	const removePoint = () => { points -= 1 }; 
-
+	const toggleControls = () => { showControls = !showControls }
 </script>
 
 <style>
@@ -13,15 +14,25 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+	h3 {
+		margin-bottom: 10px;
+	}
 </style>
 
 <div class="container">
 	<div class="card">
-		<h1>{name}</h1>
-		<h3>{points}</h3>
-		<button class="btn" on:click={addPoint}>+1</button>
-		<button class="btn btn-dark" on:click={removePoint}>-1</button>
-		<input type="number" bind:value={points}>
+		<h1>
+			{name}
+			<button class="btn btn-sm" on:click={toggleControls}>
+				{#if showControls}-{:else}+{/if}
+			</button>
+		</h1>
+		<h3>Points: {points}</h3>
+		{#if showControls}
+			<button class="btn" on:click={addPoint}>+1</button>
+			<button class="btn btn-dark" on:click={removePoint}>-1</button>
+			<input type="number" bind:value={points}>
+		{/if}
 	</div>
 </div>
 
